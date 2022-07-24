e4af068b22ed9c84bfdc7d9a16f30d5d6b445efa
ImYurikhan <shiine-31@hotmail.fr>
2022-07-24 17:27:51 +0200

    Second commit

--- loto.py
+++ loto.py
@@ -10,38 +10,32 @@ parser = argparse.ArgumentParser(description='Loto & euromillions game')
 parser.add_argument('-e','--euromillions',dest='euromillions',action='store_true',help='Choose Euromillions mode')
 parser.add_argument('-l','--loto',dest='loto',action='store_true',help='Choose Loto mode')
 args = parser.parse_args()
+
 numbers = []
 stars = []
 rand_num = random.randrange(1,49)
 
-
-
 def euromillions(nb_stars):
 
-	z = """     
+	z = """
  			Tirage en cours ....
-        
         [+]█████████████████████████████████████████████████[+]
 
 """
-
 	for c in z:
 		sys.stdout.write(c)
 		sys.stdout.flush()
 		time.sleep(0.02)
-
 	for num in range(5):
 		add_num_to_list = numbers.append(rand_num)
 		if numbers.count(rand_num) > 1:
-			multiple_numbers()	
-	print(f'Numbers: {numbers}')
-	
+			multiple_numbers()
 	for star in range(nb_stars):
 		rand_star = random.randrange(1,12)
 		stars.append(rand_star)
 		if stars.count(rand_num) > 1:
 			multiple_numbers()
-	print(f'Stars: {stars}')
+	return numbers,stars
 
 def multiple_numbers():
 	ind = numbers.index(rand_num)
@@ -54,7 +48,8 @@ def loto():
 
 if args.euromillions:
 	euromillions(2)
+	print(f'Numbers: {numbers}\nStars: {stars}')
+
 elif args.loto:
 	loto()
-else:
-	sys.exit('No args...')
+	print(f'Numbers: {numbers}\nStars: {stars}')
